066379dbae2fb95409b104ddd8a2b1c6a4c8c1f1
CR_Avila <carlosrobertoavilah@gmail.com>
2019-01-16 14:54:55 -0600

    Add epileptic effect

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
-const walk = 50;
+const walk = 100;
 
 
 function shadow(e) {
@@ -16,7 +16,12 @@ function shadow(e) {
     const yWalk = Math.round((y / height * walk) - (walk / 2));
     console.log(xWalk, yWalk);
 
-    text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(0,0,0,0.1)`
+    text.style.textShadow = `
+      ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+      ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
+      ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
+      ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
+    `;
 }
 
 hero.addEventListener('mousemove', shadow);
